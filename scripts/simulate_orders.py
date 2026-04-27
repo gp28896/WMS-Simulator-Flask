bfbf463efabb475833ce313a651042f1db9215aa
gp28896 <gp28896@gmail.com>
2026-04-27 17:15:56 +0530

    updated import, calling create instead of allocate order

--- scripts/simulate_orders.py
+++ scripts/simulate_orders.py
@@ -1,30 +1,29 @@
 import random
-from app.services.order_service import order_service
+from app.services.order_service import OrderService
 from app.services.allocation_service import AllocationService
 
 
 order_service = OrderService()
 allocation_service = AllocationService()
 
-
 SKUS = ["SKU1", "SKU2", "SKU3"]
 
 
-def simulate(n = 20):
-	for _ in range(n):
-		items = [{
-			"sku": random.choice(SKUS),
-			"quantity": random.randind(1, 5)
-		}]
+def simulate(n=10):
+    for _ in range(n):
+        items = [{
+            "sku": random.choice(SKUS),
+            "quantity": random.randint(1, 5)
+        }]
 
-		order_id = order_service.allocate(items)
-		
-		try:
-			allocation_service.allocate(items)
-			print(f"Order {order_id} allocated")
-		except Exception as e:
-			print(f"Order {order_id} failed to allocate. {e}")
+        order_id = order_service.create_order(items)
+
+        try:
+            allocation_service.allocate(order_id, items)
+            print(f"Order {order_id} allocated")
+        except Exception as e:
+            print(f"Order {order_id} failed: {e}")
 
 
 if __name__ == "__main__":
-	simulate()
+    simulate()
